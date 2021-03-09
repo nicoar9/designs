171cc37d4cb5944ab2b408b590b69891eb511d60
Nicholas Abad <nicoar9@gmail.com>
2021-03-09 09:39:44 -0500

    :rocket:Deploy: Creates basic flutter template

---
+++ lib/src/widgets/headers.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class SquareHeader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      color: Color(0xff615AAB),
+    );
+  }
+}
+
+class RoundedBorderHeader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      decoration: BoxDecoration(
+        color: Color(0xff615AAB),
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(70),
+          bottomRight: Radius.circular(70),
+        ),
+      ),
+    );
+  }
+}
